52afedd119007ec421ace1ba79a96c9d03060466
xavier@DellXPSA <xavier@DellXPSA>
2023-09-12 20:01:20 +0200

    div and minus opretaions added

--- inter/eval.sp.ariths.go
+++ inter/eval.sp.ariths.go
@@ -7,8 +7,14 @@ var ErrNaN = fmt.Errorf("term is not a number")
 func init() {
 	Register("+", true, spPlus)
 	Register("*", true, spTimes)
+	Register("/", true, spDiv)
+	Register("-", true, spMinus)
 }
 
+// ( + ) - 0
+// ( + n ) - n
+// ( + n m ) - n + m
+// ( + n m p ) - n + m + p
 func spPlus(it *Inter, args Term) Term {
 
 	n := car(args)
@@ -33,10 +39,45 @@ func spPlus(it *Inter, args Term) Term {
 	}
 }
 
+// ( - ) -> 0
+// ( - n ) -> -n
+// ( - n m ) -> n-m
+// ( - n m p q ) ->
+func spMinus(it *Inter, args Term) Term {
+
+	n := car(args)
+
+	if n == nil || (n == Pair{}) { // ( + ) evaluates to 0
+		return NumberZero
+	}
+
+	if !n.IsNumber() {
+		return Error{ErrNaN, args}
+	}
+
+	if cdr(args) == nil {
+		return NumberZero.Minus(n.(Number)) // -n
+	}
+
+	m := spPlus(it, cdr(args)) // not a mistake, because - is associative left to right
+	if m.IsNumber() {
+		return n.(Number).Minus(m.(Number))
+	} else {
+		return Error{ErrNaN, args}
+	}
+}
+
+// ( * ) - 1
+// ( * n ) - n
+// ( * n m ) - nm
 func spTimes(it *Inter, args Term) Term {
 
 	n := car(args)
-	if n == nil || !n.IsNumber() {
+	if n == nil || (n == Pair{}) {
+		return NumberOne
+	}
+
+	if !n.IsNumber() {
 		return Error{ErrNaN, args}
 	}
 
@@ -47,6 +88,32 @@ func spTimes(it *Inter, args Term) Term {
 	m := spTimes(it, cdr(args))
 	if m.IsNumber() {
 		return n.(Number).Mul(m.(Number))
+	} else {
+		return Error{ErrNaN, args}
+	}
+}
+
+// ( / n ) - 1/n
+// ( / n m ) - n/m
+// ( / n  m p q ) - n/mpq
+// ( / ) - 1
+func spDiv(it *Inter, args Term) Term {
+
+	n := car(args)
+	if n == nil || (n == Pair{}) {
+		return NumberOne
+	}
+	if !n.IsNumber() {
+		return Error{ErrNaN, args}
+	}
+
+	if cdr(args) == nil {
+		return NumberOne.Div(n.(Number))
+	}
+
+	m := spTimes(it, cdr(args)) // Not an error, because / associates from LEFT to RIGHT
+	if m.IsNumber() {
+		return n.(Number).Div(m.(Number))
 	} else {
 		return Error{ErrNaN, args}
 	}
