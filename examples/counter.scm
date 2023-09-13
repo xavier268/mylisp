c6e16c19ee53d1e6400d76e6e03696ac9238c45f
xavier@DellXPSA <xavier@DellXPSA>
2023-09-13 12:47:47 +0200

    tests ok, example added

--- examples/counter.scm
+++ examples/counter.scm
@@ -2,19 +2,20 @@
 ; demo implementing a counter with closure ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-( define make-counter                       ; make or reset counter by redefining the counter procedure
-                                            ; we quote the definition of counter to delay actual creation
-        '(define counter                    ; counter will increment the created counter
-            (let ((count 0))                ; Initialize count to 0 within the closure
+( define make-counter                               ; make or reset counter by redefining the counter procedure
+                                                    ; we quote the definition of counter to delay actual creation 
+                                                    ; to when make-counter will be called
+        '(define counter                            ; counter points to a procedure that increments counter
+            (let ((count 0))                        ; Initialize count to 0 within the closure
                 ( display "counter created/reset")
                 (newline)   
-                (lambda ()                  ; Return a closure that can be used to access and update count
-                (set! count (+ count 1))    ; Increment count by 1
-                count                       ; Return the current count
-                )                           ; end of lambda definition
-            )                               ; end of let
-        )                                   ; end of define    
-)                                           ; end of reset-counter          
+                (lambda ()                          ; creates a procedure/closure that can be used to access and update count
+                    (set! count (+ count 1))        ; Increment count by 1
+                    count                           ; procedure will return the current count
+                )                                   ; end of lambda definition
+            )                                       ; end of let, returns the last argument, the procedure just constructed
+        )                                           ; end of counter define, counter points to the procedure just constructed   
+)                                                   ; end of make-counter definition     
 
 ; calling counter should fail, because none created yet
 ( display "calling counter should fail, because not yet created " ) ( newline)
@@ -34,5 +35,8 @@
 ( display (counter)) (newline)
 ( display (counter)) (newline)
 
-(quit)
+( display "let's confirm that count is not accessible from outisde the closure ?") ( newline)
+count 
+( count )
+
 
